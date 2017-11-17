5dc5b88221e82230170326da6d0d94ab4fcf4681
Zach Taylor <taylorzr@gmail.com>
2017-11-17 09:54:42 -0600

    Splitup main function

--- main.go
+++ main.go
@@ -9,10 +9,53 @@ import(
 	"github.com/taylorzr/hibye/storage"
 )
 
-func main() {
-	old_users_path := "old_users.csv"
-	// old_users_path := "test_old_users.csv"
+const old_users_path = "old_users.csv"
+// const old_users_path = "test_old_users.csv"
 
+func check() {
+	oldUsers, err := storage.Read(old_users_path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newUsers, err := hipchat.GetAllUsers()
+	// newUsers, err := storage.Read("test_new_users.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer storage.Write(old_users_path, newUsers)
+
+	hiredUsers := compare.FindFired(oldUsers, newUsers)
+	firedUsers := compare.FindHired(oldUsers, newUsers)
+
+	err = notify.Notify(hiredUsers, firedUsers)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initialize() {
+	newUsers, err := hipchat.GetAllUsers()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = storage.Write(old_users_path, newUsers)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = hipchat.SendMessage("HiBye initialized, will report on next run", hipchat.Yellow)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
 	dataExists, err := storage.Exists(old_users_path)
 
 	if err != nil {
@@ -20,42 +63,8 @@ func main() {
 	}
 
 	if dataExists {
-		oldUsers, err := storage.Read(old_users_path)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		newUsers, err := hipchat.GetAllUsers()
-		// newUsers, err := storage.Read("test_new_users.csv")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		defer storage.Write(old_users_path, newUsers)
-
-		hiredUsers := compare.FindFired(oldUsers, newUsers)
-		firedUsers := compare.FindHired(oldUsers, newUsers)
-
-		err = notify.Notify(hiredUsers, firedUsers)
-		if err != nil {
-			log.Fatal(err)
-		}
+		check()
 	} else {
-		newUsers, err := hipchat.GetAllUsers()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = storage.Write(old_users_path, newUsers)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = hipchat.SendMessage("HiBye initialized, will report on next run", hipchat.Yellow)
-
-		if err != nil {
-			log.Fatal(err)
-		}
+		initialize()
 	}
 }

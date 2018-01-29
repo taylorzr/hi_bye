3f1eeb1654fb420c690aba35e9318b3ca8d9439f
Zach Taylor <taylorzr@gmail.com>
2018-01-29 13:52:33 -0600

    Add notes

--- main.go
+++ main.go
@@ -49,6 +49,12 @@ func check() {
 	newUsers, err := hipchat.GetAllUsers()
 	// newUsers, err := storage.Read("test_new_users.csv")
 	if err != nil {
+		// TODO:
+		// Sometimes get an error written to stderr like:
+		// 2018/01/24 10:04:05 Get https://api.hipchat.com/v2/user?max-results=1000&auth_token=ZqDKnJaxyiK7wEScsmMW2ad6yHD81P13Tl10Kwjr: dial tcp: lookup api.hipchat.com: no such host
+		// This is because the laptop has no network connection, it would be
+		// nice to only log when a successful run of this program hasn't been
+		// made in the last few hours or something
 		log.Fatal(err)
 	}
 
